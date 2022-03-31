eddea3ff0adca48b68546c9fbf6235c22e29ce70
bao2206 <baopro2206@gmail.com>
2022-03-31 23:49:21 +0700

    baooo

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ document.getElementById("btnThemSo").onclick = function () {
   var number = document.getElementById("txtArray").value * 1;
 
   a.push(number);
-  console.log(a);
+  getEle("footerArray").innerHTML ="["+ a+"]";
 };
 
 function getEle(id) {
   return document.getElementById(id);
 }
 
-
-//Câu 1 
+//Câu 1
 getEle("btnTongSoDuong").onclick = function () {
   var sum = 0,
     i,
@@ -86,54 +85,90 @@ getEle("btnSapXepTang").addEventListener("click", function () {
     "Mảng sắp xếp theo thứ tự tăng dần" + a;
 });
 //Câu 8
-function snt(num) {
-  if (num < 2) return 0;
-  for (var i = 2; i < num; i++) {
-    if (num % i === 0) return false;
-    return true;
-  }
-}
-
 
 getEle("btnFirstPrime").onclick = function () {
-    var num = 0 ;
-    var kq = '';
-    for (var i = 0; i < a.length - 1; i++) {
-        if (snt(a[i]) === true) num = a[i];
-        else num = -1;
+  var kq = 0;
+  for(i = 0; i < a.length; i++){
+    var count = 0;
+    var check = a[i];
+    for(j = 2; j <= Math.sqrt(check); j++){
+      if(check % j === 0){
+        count++;
+      }
     }
-    kq += "Số nguyên tố đầu tiên có trong mảng là " +num;
-    
-    document.getElementById("footerFirstPrime").innerHTML = kq;
-
+    if(count === 0 && check > 1){
+      kq +=  check;
+      break;
+    }else if(count === 1){
+      kq = -1;
+    }
+  }
+  getEle("footerFirstPrime").innerHTML = kq;
 };
 
+//Câu 4
 
-//Câu 4 
+getEle("btnFindMinPos").onclick = function () {
+  var kq = "";
+  var minPos = 0;
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] > 0) {
+      minPos = a[i];
+      break;
+    }
+  }
+  if (minPos != 0) {
+    for (var i = 0; i < a.length; i++)
+      if (a[i] > 0 && a[i] < minPos) {
+        minPos = a[i];
+        kq += "Số dương nhỏ nhất là " + minPos;
+      }
+  } else {
+    kq += "Trong mảng này không có phần tử nào là số dương";
+  }
+  getEle("footerMinPos").innerHTML = kq;
+};
 
+//Câu 9
 
-getEle("btnFindMinPos").onclick = function(){
-    var kq = '';
-    var minPos = 0;
-    for(var i = 0; i < a.length; i++)
-    {
-        if(a[i] > 0) 
-        {
-            minPos = a[i];
-            break;
-        }
-    }
-    if(minPos != 0)
-    {
-        for(var i = 0; i < a.length; i++)
-        if(a[i] > 0 && a[i] < minPos)
-        {
-            minPos = a[i];
-            kq += "Số dương nhỏ nhất là "+minPos;
-        }
-    }
-    else {
-        kq +="Trong mảng này không có phần tử nào là số dương";
-    }
-    getEle("footerMinPos").innerHTML = kq;
+getEle("btnN").onclick = function () {
+  var count = 0;
+
+  for (var i = 0; i < a.length; i++) {
+    if (Number.isInteger(a[i]) == true) count ++;
+  }
+
+  getEle("footerSoNguyen").innerHTML = count;
+};
+
+//Câu 6
+getEle("btnSwap").onclick = function(){
+  var input_1 = getEle("txt1").value * 1;
+  var input_2 = getEle("txt2").value * 1;
+  var temp =  a[input_1];
+  var res = '';
+  if(input_1 < 0 || input_1 >=a.length) result = false;
+  else if(input_2 < 0 || input_2 >=a.length) result = false;
+  else{
+    a[input_1] = a[input_2];
+    a[input_2] = temp;
+    res = a.join("");
+  }
+  getEle("footerSwap").innerHTML = res;
+}
+//Câu 10
+
+getEle("btnCompare").onclick = function(){
+  var countPositive = 0,  countNegative = 0, result = '', i;
+  for(i = 0; i < a.length; i++)
+  {
+    if(a[i] > 0) countPositive ++;
+    else countNegative ++;
+  } 
+
+  if(countPositive === countNegative) result = "Số dương bằng số âm";
+  else if(countPositive > countNegative) result = "Số dương lớn hơn số âm";
+  else result = "Số âm lớn hơn số dương";
+  getEle("footerCompare").innerHTML = result;
+
 }
